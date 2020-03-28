38fea8a701afccb64d82c4900339d032bcffaeb5
Yana <savchenkoyana97@gmail.com>
2020-03-28 22:58:23 +0700

    Fixed browser cache bug

--- fileuploader.py
+++ fileuploader.py
@@ -8,13 +8,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from pathlib import Path
+import time
 
-UPLOAD_FOLDER = '/home/yana/Documents/temp'
+UPLOAD_FOLDER = 'static/audios'
+SAVE_FOLDER = 'static/images'
 ALLOWED_EXTENSIONS = set(['mp3'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SAVE_FOLDER'] = SAVE_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -32,7 +35,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+            return render_template('result_template.html', mp3_title=filename, image_name= ( plot_from_mp3(filename)+'?%d'%(time.time()) ) )
         else:
             flash('Invalid type')
     return render_template('upload_template.html')
@@ -42,17 +45,18 @@ from flask import send_from_directory
 def plot_from_mp3(filename):
     my_filename = UPLOAD_FOLDER + '/' + filename
     y, sr = librosa.load(my_filename)
+    os.remove(my_filename)
     D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
     librosa.display.specshow(librosa.amplitude_to_db(D,ref=np.max), y_axis='log', x_axis='time')
     plt.title('Power spectrogram')
     plt.colorbar(format='%+2.0f dB')
     plt.tight_layout()
-    plt.savefig(UPLOAD_FOLDER+"/temp.png")
-    return 'temp.png'
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], plot_from_mp3(filename))
+    path_to_png = SAVE_FOLDER + '/result.png'
+    if os.path.isfile(path_to_png):
+        os.remove(path_to_png)   
+    plt.savefig(path_to_png)
+    plt.clf()
+    return path_to_png
 
 if __name__ == '__main__':
     app.run(debug=False)
