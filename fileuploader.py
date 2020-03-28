40a9d095b46515ae00233224381c1fcc93b6f072
Yana <savchenkoyana97@gmail.com>
2020-03-28 12:07:27 +0700

    now it works with user's files

--- fileuploader.py
+++ fileuploader.py
@@ -7,9 +7,10 @@ import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from pathlib import Path
 
 UPLOAD_FOLDER = '/home/yana/Documents/temp'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3'])
+ALLOWED_EXTENSIONS = set(['mp3'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -32,32 +33,26 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file', filename=filename))
-            #return uploaded_file(filename)
         else:
             flash('Invalid type')
     return render_template('upload_template.html')
     
 from flask import send_from_directory
 
-def plot_from_mp3(D):
-    librosa.display.specshow(librosa.amplitude_to_db(D,ref=np.max),y_axis='log', x_axis='time')
+def plot_from_mp3(filename):
+    my_filename = UPLOAD_FOLDER + '/' + filename
+    y, sr = librosa.load(my_filename)
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+    librosa.display.specshow(librosa.amplitude_to_db(D,ref=np.max), y_axis='log', x_axis='time')
     plt.title('Power spectrogram')
     plt.colorbar(format='%+2.0f dB')
     plt.tight_layout()
     plt.savefig(UPLOAD_FOLDER+"/temp.png")
-    #plot = Image.open(UPLOAD_FOLDER+"/temp.png")
-    return "temp.png"
+    return 'temp.png'
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    #MyFile = UPLOAD_FOLDER + filename
-    y, sr = librosa.load(librosa.util.example_audio_file())
-    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
-    #return render_template('result_template.html', image_title="LOL", image_name=plot_from_mp3(D))
-    #return render_template('result_template.html', image_title="LOL", \
-    #       image_name=send_from_directory(app.config['UPLOAD_FOLDER'],plot_from_mp3(D)))
-    return send_from_directory(app.config['UPLOAD_FOLDER'], plot_from_mp3(D))
-#send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], plot_from_mp3(filename))
 
 if __name__ == '__main__':
     app.run(debug=False)
